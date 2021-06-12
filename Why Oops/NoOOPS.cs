ac60766e020483d978739d9cacdc594500d9886c
richrd77@gmail.com <richrd77@gmail.com>
2021-06-12 20:48:36 +0530

    j

---
+++ Why Oops/NoOOPS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Why_Oops
+{
+    class NoOOPS
+    {
+        public void Display()
+        {
+            string playerName1 = "Dhoni";
+            int playerAge1 = 34;
+
+            string playerName2 = "Kholi";
+            int playerAge2 = 24;
+
+            string playerName3 = "Zhaheer";
+            int playerAge3 = 44;
+
+            Console.WriteLine("{0} with Age {1}", playerName1, playerAge1);
+            Console.WriteLine("{0} with Age {1}", playerName2, playerAge2);
+            Console.WriteLine("{0} with Age {1}", playerName3, playerAge3);
+        }
+    }
+}
---
+++ Why Oops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Why_Oops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NoOOPS noOOpsObj = new NoOOPS();
+            noOOpsObj.Display();
+            Console.ReadKey();
+        }
+    }
+}
